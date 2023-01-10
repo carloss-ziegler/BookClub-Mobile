c78195bafcc6742c613a809dae794e4343398dbf
carloss-ziegler <chziegler445@gmail.com>
2023-01-10 08:45:38 -0300

    update

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    "http://192.168.0.18:3001" || "https://bookclub-api-ctcw.onrender.com",
+    "http://192.168.0.15:3001" || "https://bookclub-api-ctcw.onrender.com",
   withCredentials: true,
 });
